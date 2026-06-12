9834eccac4ac5fd6ef6ac92c084294262bc71378
Alex Patton <alex.patton@ozeanmedia.com>
2026-06-12 12:00:04 -0400

    Update codeneighborhoodtohistoric.sql

--- Berlin/codeneighborhoodtohistoric.sql
+++ Berlin/codeneighborhoodtohistoric.sql
@@ -1,16 +1,23 @@
 CASE 
-  -- 1. FIX THE CENTER NEIGHBORHOODS BY EXACT NAME
+  -- 1. THE SPLIT DISTRICTS (Explicitly by Name to bypass any ID bugs)
+  -- Mitte Splits
+  WHEN "name" = 'Mitte' THEN 'Russian'
   WHEN "name" IN ('Wedding', 'Gesundbrunnen') THEN 'French'
   WHEN "name" IN ('Moabit', 'Tiergarten', 'Hansaviertel') THEN 'British'
-  WHEN "name" = 'Mitte' THEN 'Russian'
-
-  -- 2. RUN THE REST OF THE AUTOMATIC DISTRICT CODE LOGIC
-  WHEN substr("schlüssel", 7, 2) = '12' THEN 'French'
-  WHEN substr("schlüssel", 7, 2) IN ('06', '07', '08') THEN 'US'
-  WHEN substr("schlüssel", 7, 4) = '0202' THEN 'US'
-  WHEN substr("schlüssel", 7, 2) IN ('04', '05') THEN 'British'
-  WHEN substr("schlüssel", 7, 2) IN ('03', '09', '10', '11') THEN 'Russian'
-  WHEN substr("schlüssel", 7, 4) = '0201' THEN 'Russian'
   
+  -- Friedrichshain-Kreuzberg Splits
+  WHEN "name" = 'Friedrichshain' THEN 'Russian'
+  WHEN "name" = 'Kreuzberg' THEN 'US'
+  
+  -- Charlottenburg-Wilmersdorf Splits
+  WHEN "name" IN ('Charlottenburg', 'Charlottenburg-Nord', 'Westend') THEN 'British'
+  WHEN "name" IN ('Wilmersdorf', 'Schmargendorf', 'Grunewald', 'Halensee') THEN 'US'
+
+  -- 2. THE HOMOGENEOUS DISTRICTS (Safe to use the 2-digit prefix for the rest)
+  WHEN substr("schlüssel", 7, 2) = '12' THEN 'French'         -- Reinickendorf
+  WHEN substr("schlüssel", 7, 2) IN ('06', '07', '08') THEN 'US' -- Steglitz-Zehlendorf, Tempelhof-Schöneberg, Neukölln
+  WHEN substr("schlüssel", 7, 2) = '05' THEN 'British'        -- Spandau
+  WHEN substr("schlüssel", 7, 2) IN ('03', '09', '10', '11') THEN 'Russian' -- Pankow, Treptow-Köpenick, Marzahn-Hellersdorf, Lichtenberg
+
   ELSE 'Unknown'
 END
